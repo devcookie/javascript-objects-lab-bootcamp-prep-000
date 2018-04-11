c64be14e22b05dd787af989b8ce3b6a87d108027
Catherine Lee <lee.catherine77@gmail.com>
2018-04-11 23:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newRecipe = Object.assign(object, key, value)
+  var newRecipe = Object.assign(object, [key], value)
   return newRecipe
 }
