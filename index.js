84ec2813281e2308fb676651a78e24fbf61e7b64
Catherine Lee <lee.catherine77@gmail.com>
2018-04-12 13:20:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObject = delete object.key
+  var newObject = delete object['key']
+  /*  Notes for me from elsewhere: object.key and object[key] accomplish the same thing.
+      However, object.key only works if the key name is hardwired ( I mean not happening dynamically since it cannot change at run-time). It also does not work when the key is a number instead of a string.
+
+      In other words, object[key] is more versatile.
+
+      One of the reasons we use object.key is because it is easier to type and it looks cooler.
+
+      If you can write the direct name of the key and it is not a number you can use object.key
+
+      If the key is not yet known when we write the code, then we must use object[key] but be careful, sometimes we need to wrap the key in quotes:
+      object["key"] like when the key is not a variable.*/
   return newObject
+}
+
+function destructivelyDeleteFromObjectsByKey(object, key) {
+  
 }
