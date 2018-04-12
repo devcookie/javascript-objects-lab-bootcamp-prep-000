5e001d7c4f5b30a2e28c05d14924fa47adc56f48
Catherine Lee <lee.catherine77@gmail.com>
2018-04-12 13:15:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, {[key]: value})
 }
 
+function deleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
